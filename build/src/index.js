fce9d307272bfd1fd69ca9e9d083fdf789a2e1de
Odin Thif <mjd.thif@icloud.com>
2024-11-04 00:49:47 +0100

    fixing the db not included in the bulild because of the tsc builder not including none ts or tsx files

--- build/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-(0, app_1.createApp)().listen(3000, () => {
-  console.log(`Server started on http://localhost:3000.`);
-});
-//# sourceMappingURL=index.js.map
