410b320a6a559031fd87a4fdd3ae2b4d94aead26
Odin Thif <mjd.thif@icloud.com>
2024-11-04 00:57:33 +0100

    removing the verifyLogin for fetching posts

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -10,7 +10,7 @@ import { verifyLogin } from "../middleware";
 
 export const postRouter: express.Router = express.Router();
 postRouter.post("/post", verifyLogin, createPost);
-postRouter.get("/posts", verifyLogin, getPosts);
+postRouter.get("/posts", getPosts);
 postRouter.patch("/posts/:id", verifyLogin, updatePost);
 postRouter.delete("/posts/:id", verifyLogin, deletePost);
 postRouter.get("/posts/:authorid", verifyLogin, getPostsByAuthor);
