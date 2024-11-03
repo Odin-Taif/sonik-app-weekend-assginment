8912b8d54fa763ff521ee3bf97239389a9316d7b
Odin Thif <mjd.thif@icloud.com>
2024-11-03 11:17:36 +0100

    protucting the update post with verifylogin middleware

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -5,4 +5,4 @@ import { verifyLogin } from "../middleware";
 export const postRouter: express.Router = express.Router();
 postRouter.post("/post", verifyLogin, createPost);
 postRouter.get("/posts", getPosts);
-postRouter.patch("/posts/:id", updatePost);
+postRouter.patch("/posts/:id", verifyLogin, updatePost);
