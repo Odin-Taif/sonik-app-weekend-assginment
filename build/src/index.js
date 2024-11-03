647bb4a1e7df490aacffab3440af1b6c71abe483
Odin Thif <mjd.thif@icloud.com>
2024-11-03 23:56:53 +0100

    chnage drizzle path

--- build/src/index.js
+++ build/src/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 (0, app_1.createApp)().listen(3000, () => {
-    console.log(`Server started on http://localhost:3000.`);
+  console.log(`Server started on http://localhost:3000.`);
 });
 //# sourceMappingURL=index.js.map
